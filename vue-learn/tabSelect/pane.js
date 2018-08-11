ad6030b9bd5d16165d2477336cc10db653ce9ce8
Vxee <15258691200@163.com>
2018-08-11 10:24:11 +0800

    add tabSelect component

---
+++ vue-learn/tabSelect/pane.js
@@ -0,0 +1,38 @@
+Vue.component('pane', {
+    name: 'pane',
+    template: '\
+        <div class="pane" v-show="show"> \
+            <slot></slot> \
+        </div>',
+    data: function(){
+        return {
+            show: true
+        }
+    },
+    props: {
+        name: {
+            type: String
+        },
+        label: {
+            type: String,
+            default: ''
+        },
+        close: {
+            type: String,
+            default: 'false'
+        }
+    },
+    methods: {
+        updateNav(){
+            this.$parent.updateNav();
+        }
+    },
+    watch: {
+        label(){
+            this.updateNav();
+        }
+    },
+    mounted() {
+        this.updateNav();
+    },
+})
